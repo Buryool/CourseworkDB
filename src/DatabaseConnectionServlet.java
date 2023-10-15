03e8229642e8297fa91d843f3e273726542dac9f
whb <whb@gmail.com>
2023-10-16 00:55:28 +0800

    优化了项目结构 考虑了访问数据库时线程并发的安全问题，采用读者写者模型设计 完善了“查”对应的Servlet

--- src/DatabaseConnectionServlet.java
+++ src/DatabaseConnectionServlet.java
@@ -1,11 +1,10 @@
-package Final;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.Semaphore;
 
 /**
  * 存储该context对应的数据库信息，
@@ -26,6 +25,13 @@ public class DatabaseConnectionServlet extends HttpServlet {
         this.connection = DBUtil.getLocalConnection();
         // 设置context对应的数据库
         context.setAttribute("dbConnection", connection);
+        // 用于readerCount的互斥访问
+        context.setAttribute("mutex", new Semaphore(1));
+        // 用于统计读者数量
+        context.setAttribute("readerCount", 0);
+        // 用于读者和写者互斥
+        context.setAttribute("rw", new Semaphore(1));
+        // TODO 在这里有时间可以试试如果给context加入变量后原始Servlet销毁，那context中的变量还能不能继续访问
     }
 
     @Override
--- src/Servlet_Query.java
+++ src/Servlet_Query.java
@@ -1,13 +1,11 @@
-package Final;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Enumeration;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,8 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/AllInfo")
-public class Servlet01 extends HttpServlet {
+@WebServlet("/Query")
+public class Servlet_Query extends HttpServlet {
     private Connection c;
 
     // 在Servlet初始化的时候获取数据库连接，用来在本类中操作数据库
@@ -28,16 +26,36 @@ public class Servlet01 extends HttpServlet {
         System.out.println("数据库连接：" + c);
     }
 
-    private String exeQuery(String query){
+    private String executeQuery(String query){
         String res;
         Statement statement;
+        ServletContext context = getServletContext();
+        Semaphore rw = (Semaphore) context.getAttribute("rw");
+        Semaphore mutex = (Semaphore) context.getAttribute("mutex");
+        int readerCount = (int) context.getAttribute("readerCount");
 
         try {
             statement = c.createStatement();
+            // 第一个读进程加锁
+            mutex.acquire();
+            if (readerCount == 0){
+                rw.acquire();
+            }
+            readerCount++;
+            mutex.release();
+            // 读
             ResultSet resultSet = statement.executeQuery(query);
+            // 最后一个读进程解锁
+            mutex.acquire();
+            readerCount--;
+            if (readerCount == 0){
+                rw.release();
+            }
+            mutex.release();
+
             List<List> result = DBUtil.getResult(resultSet);
             res = DBUtil.listToString(result);
-        } catch (SQLException e) {
+        } catch (SQLException | InterruptedException e) {
             throw new RuntimeException(e);
         }
 
@@ -48,32 +66,25 @@ public class Servlet01 extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
 
         String sql = request.getParameter("sql");
-        System.out.println("SQL信息：" + sql);
+        System.out.println("SQL语句：" + sql);
 
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>数据库全部信息</title>");
+            out.println("<title>查询结果</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>" + exeQuery(sql) + "</h1>");
+            out.println("<h1>" + executeQuery(sql) + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        System.out.println("doGet方法被调用！");
-        processRequest(request, response);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        System.out.println("doPost方法被调用！");
+        System.out.println("DB的doPost方法被调用！");
         processRequest(request, response);
     }
 }
